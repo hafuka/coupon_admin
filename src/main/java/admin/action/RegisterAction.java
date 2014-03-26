3ecb4e9726b32606bb4f0facd27132109d75290b
Satoshi Hafuka <hafuka_satoshi_gn@cyberagent.co.jp>
2014-03-26 16:54:16 +0900

    かいはつ

--- src/main/java/admin/action/RegisterAction.java
+++ src/main/java/admin/action/RegisterAction.java
@@ -2,21 +2,19 @@ package admin.action;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.seasar.mai.mail.MailAddress;
 import org.seasar.struts.annotation.Execute;
 
 import admin.dto.RegistMailDto;
 import admin.mai.MailMai;
 import admin.service.AdminService;
-import coupon.service.ShopService;
-import coupon.util.CryptUtils;
 
 public class RegisterAction extends BaseAction {
 
 	@Resource
 	protected AdminService adminService;
-	@Resource
-	protected ShopService shopService;
+
 
 	@Resource
 	protected MailMai mailMai;
@@ -30,12 +28,16 @@ public class RegisterAction extends BaseAction {
 
 	@Execute(validator=false)
 	public String regist() throws Exception {
-		Integer shopId = shopService.findNextShopId();
-		String password = CryptUtils.encrypt(shopId.toString());
+		// 6桁のランダム文字列をログインパスワードとする
+		String password = RandomStringUtils.randomAlphanumeric(6);
 		RegistMailDto mailDto = new RegistMailDto();
 		mailDto.to = new MailAddress[]{new MailAddress(email)};
 		mailDto.loginPassword = password;
 		mailMai.sendRegistMail(mailDto);
+
+		// 管理者認証情報登録
+		adminService.regist(email, password);
+
 		return "regist_result.ftl";
 	}
 }
